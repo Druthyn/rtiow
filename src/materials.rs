17ff10d4dd253e103f3860775cf72f7ffa5b76e3
Jadzia -ubuntu laptop <jadzia_ubuntu@laptop.rock>
2023-03-05 01:12:25 +0000

    Added support for materials: Lambertian and Metal

---
+++ src/materials.rs
@@ -0,0 +1,58 @@
+use crate::{ray::Ray, vec3::{Color, Vec3}, shapes::HitRecord};
+
+pub trait Scatter :Send + Sync{
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)>;
+}
+
+pub struct Lambertian {
+    albedo: Color,
+}
+
+impl Lambertian {
+    pub fn new(a: Color) -> Self {
+        Lambertian {
+            albedo: a,
+        }
+    }
+}
+
+impl Scatter for Lambertian {            
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let mut scatter_direction = rec.get_normal() + Vec3::random_unit_vector();
+
+        if scatter_direction.is_near_zero() {
+            scatter_direction = rec.get_normal();
+        }
+        let scattered = Ray::new(rec.get_p(), scatter_direction);
+        Some((self.albedo, scattered))
+    }
+}
+
+
+pub struct Metal {
+    albedo: Color,
+}
+
+impl Metal {
+    pub fn new(a: Color) -> Self {
+        Metal {
+            albedo: a,
+        }
+    }
+}
+
+impl Scatter for Metal {
+            
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let reflected = r_in.direction().reflect(&rec.get_normal()).unit_vector();
+
+        let scattered = Ray::new(rec.get_p(), reflected);
+        
+        if scattered.direction().dot(&rec.get_normal()) <= 0.0 {
+            return None
+        }
+
+        let attenuation = self.albedo;
+        Some((attenuation, scattered))
+    }
+}
