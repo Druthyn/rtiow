e455a7ec750899815389d24458a9dd8e5f25db15
Jadzia -ubuntu laptop <jadzia_ubuntu@laptop.rock>
2023-04-08 22:49:13 +0100

    Implement remaining axis rect in macro expansion

--- src/shapes/rectangles.rs
+++ src/shapes/rectangles.rs
@@ -5,72 +5,57 @@ use crate::{materials::Material, bvh::aabb::Aabb, ray::Ray, vec3::{Point3, Vec3}
 
 use super::{Hit, HitRecord};
 
-// todo: macro the duplication out of this section
-
-pub struct XyRect {
-    mp: Arc<dyn Material>,
-    xs: (f64, f64),
-    ys: (f64, f64),
-    k: f64,
-}
-
-pub struct XzRect {
-    mp: Arc<dyn Material>,
-    xs: (f64, f64),
-    zs: (f64, f64),
-    k: f64,
-}
-
-pub struct YzRect {
-    mp: Arc<dyn Material>,
-    ys: (f64, f64),
-    zs: (f64, f64),
-    k: f64,
-}
-
 macro_rules! build_rect_struct {
-    ($d1:tt, $d2:tt) => {
-        pub struct YzRect {
-            mp: Arc<dyn Material>,
-            $d1s: (f64, f64),
-            zs: (f64, f64),
-            k: f64,
+    ($d1:tt, $d2:tt plane. Normal in $d3:tt) => {
+        paste! {
+            pub struct [<$d1:upper $d2 Rect>] {
+                mp: Arc<dyn Material>,
+                [<$d1 s>]: (f64, f64),
+                [<$d2 s>]: (f64, f64),
+                k: f64,
+            }
+        }
+        paste! {
+            impl [<$d1:upper $d2 Rect>] {
+                pub fn new([<$d1 s>]: (f64, f64), [<$d2 s>]: (f64, f64), k: f64, mp: Arc<dyn Material>) -> [<$d1:upper $d2 Rect>] {
+                    [<$d1:upper $d2 Rect>] {[<$d1 s>], [<$d2 s>], k, mp}
+                }
+            }
+        }
+        paste! {            
+            impl Hit for [<$d1:upper $d2 Rect>] {
+                fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+                    let t = (self.k - r.origin().$d3()) / r.direction().$d3();
+                    if t < t_min || t > t_max {
+                        return None
+                    }
+            
+                    let $d1 = r.origin().$d1() + t*r.direction().$d1();
+                    let $d2 = r.origin().$d2() + t*r.direction().$d2();
+            
+                    if $d1 < self.[<$d1 s>].0 || $d1 > self.[<$d1 s>].1 || $d2 < self.[<$d2 s>].0 || $d2 > self.[<$d2 s>].1 {
+                        return None
+                    }
+            
+                    let u = ($d1-self.[<$d1 s>].0)/(self.[<$d1 s>].1-self.[<$d1 s>].0);
+                    let v = ($d2-self.[<$d2 s>].0)/(self.[<$d2 s>].1-self.[<$d2 s>].0);
+                    let outward_normal = &Vec3::new(0,0,1);
+                    let p = r.at(t);
+            
+                    Some(HitRecord::new(p, t, u, v, r, outward_normal, self.mp.clone()))
+                }
+            
+                fn bounding_box(&self, _time0: f64, _time1: f64) -> Option<Aabb> {
+                    Some(Aabb::new(
+                        Point3::new(self.[<$d1 s>].0, self.[<$d1 s>].1, self.k-0.0001), 
+                        Point3::new(self.[<$d2 s>].0, self.[<$d2 s>].1, self.k+0.0001)
+                    ))
+                }
+            }
         }
     };
 }
 
-impl XyRect {
-    pub fn new(xs: (f64, f64), ys: (f64, f64), k: f64, mp: Arc<dyn Material>) -> XyRect {
-        XyRect {xs, ys, k, mp}
-    }
-}
-
-impl Hit for XyRect {
-    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
-        let t = (self.k - r.origin().z()) / r.direction().z();
-        if t < t_min || t > t_max {
-            return None
-        }
-
-        let x = r.origin().x() + t*r.direction().x();
-        let y = r.origin().y() + t*r.direction().y();
-
-        if x < self.xs.0 || x > self.xs.1 || y < self.ys.0 || y > self.ys.1 {
-            return None
-        }
-
-        let u = (x-self.xs.0)/(self.xs.1-self.xs.0);
-        let v = (y-self.ys.0)/(self.ys.1-self.ys.0);
-        let outward_normal = &Vec3::new(0,0,1);
-        let p = r.at(t);
-
-        Some(HitRecord::new(p, t, u, v, r, outward_normal, self.mp.clone()))
-    }
-
-    fn bounding_box(&self, _time0: f64, _time1: f64) -> Option<Aabb> {
-        Some(Aabb::new(
-            Point3::new(self.xs.0, self.xs.1, self.k-0.0001), 
-            Point3::new(self.ys.0, self.ys.1, self.k+0.0001)
-        ))
-    }
-}
+build_rect_struct!(x, y plane. Normal in z);
+build_rect_struct!(x, z plane. Normal in y);
+build_rect_struct!(y, z plane. Normal in x);
